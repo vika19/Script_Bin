6604bf6a574e84081913fb423d912e9bbc3768d0
Sheri Sanders <vika19@gmail.com>
2018-07-17 18:26:39 -0400

    Update add_user.sh

--- add_user.sh
+++ add_user.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
 #This script adds users to a system (intended for Jetstream VM) with the ability to ssh into the shell for collaborative use.
-
-#A couple of notes about the script. Anything that has a # is not read by the computer.  Strings of #'s indicate a flexible point and a note below:
-######1######) I have included a couple options in commented out lines - one at the top to allow for one or multiple users to be added.  Do not include ","s between the listed names!
-
-######2######) If you are using Ubuntu, use the first block; if you are using CentOS, use the second.
-
-######3######) If you add the people to the users group, they will likely have sudo - meaning root priviledges.  If you do not want this, you will have to make a separate group (e.g. "train") and add it to the allowable ssh groups in /etc/ssh/sshd_config.
-#              If you only want to add people for use on an Rstudio server, etc. remove the usermod statement that is flagged.
+#SEE www.ncgas.org/NCGAS_Blog.php/Adding%Users%to%Jetstream.php  up soon!
+http://ncgas.org/Blog_Posts/Installing%20conda%20packages%20locally.php
 
 declare -a names=("ncgas_admin")
-#######1######## if you want to use multiple users, use the following format:
-#declare -a names=("user1" "user2")
 
 p=temporarypasswords4thewin!
 
@@ -22,15 +14,9 @@ do
 #check if user exists
 if grep -q $i /etc/passwd; 
 	then : ; 
-	#######2######if you are using Ubuntu, use these three lines and comment out the CentOS set:
 	else adduser --disabled-password --gecos "" $i ; 
 	     echo "$i:$p" | chpasswd ;
-	     usermod -G users $i;          #######3######## This will give sudo access as well as ssh
-
-	#######2######if you are on CentOS, use the below command instead, and comment out the above Ubuntu set:	
-	#else adduser $i ;
-	     #echo "$i:$p" | chpasswd ;
-	     #usermod -G users $i;         #######3######## This will give sudo access as well as ssh
+	     usermod -G users $i;          ############### This will give sudo access as well as ssh
 fi
 
 #check if user home exists
